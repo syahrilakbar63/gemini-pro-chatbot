4855c655ec3bb826adfb0f89081ba533e7f6cfc3
Syahril Akbar <89430734+syahrilakbar63@users.noreply.github.com>
2024-01-26 13:07:46 +0000

    Refactor HTML and CSS code for improved readability and responsiveness

--- public/js/script.js
+++ public/js/script.js
@@ -1,142 +1,150 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const textarea = document.getElementById('messageTextarea');
-    const chatMessagesContainer = document.querySelector('.chat-messages');
-    const chatHistory = [];
-    let isTouchDevice = false;
+  const textarea = document.getElementById('messageTextarea')
+  const chatMessagesContainer = document.querySelector('.chat-messages')
+  const chatHistory = []
+  let isTouchDevice = false
 
-    if ('ontouchstart' in window || navigator.maxTouchPoints) {
-        isTouchDevice = true;
+  if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    isTouchDevice = true
+  }
+
+  textarea.addEventListener('input', autoExpandTextarea)
+
+  textarea.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      if (isTouchDevice) {
+        event.preventDefault()
+        insertNewLine()
+      } else if (!event.shiftKey) {
+        event.preventDefault()
+        sendMessage()
+      }
     }
+  })
 
-    textarea.addEventListener('input', autoExpandTextarea);
+  window.sendMessage = async function () {
+    const message = textarea.value.trim()
+    if (message !== '') {
+      try {
+        chatHistory.push({ role: 'user', text: message })
+        updateChatUI()
 
-    textarea.addEventListener('keydown', function (event) {
-        if (event.key === 'Enter') {
-            if (isTouchDevice) {
-                event.preventDefault();
-                insertNewLine();
-            } else if (!event.shiftKey) {
-                event.preventDefault();
-                sendMessage();
-            }
+        const response = await fetch('/sendMessage', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ userInput: message }),
+        })
+
+        if (response.ok) {
+          const data = await response.json()
+          chatHistory.push({ role: 'bot', text: 'Tunggu dulu, masih mikir' })
+          updateChatUI()
+
+          chatHistory.pop()
+          chatHistory.push({ role: 'bot', text: data.text })
+
+          updateChatUI()
+          scrollToBottom() // Scroll to bottom after the response is displayed
+        } else {
+          console.error('Failed to send message to server')
+          chatHistory.push({
+            role: 'bot',
+            text: 'Yahh error, silahkan refresh atau coba beberapa saat lagi',
+          })
+          updateChatUI()
         }
-    });
-
-    window.sendMessage = async function () {
-        const message = textarea.value.trim();
-        if (message !== '') {
-            try {
-                chatHistory.push({ role: 'user', text: message });
-                updateChatUI();
-
-                const response = await fetch('/sendMessage', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ userInput: message }),
-                });
-
-                if (response.ok) {
-                    const data = await response.json();
-                    chatHistory.push({ role: 'bot', text: 'Tunggu dulu, masih mikir' });
-                    updateChatUI();
-
-                    chatHistory.pop();
-                    chatHistory.push({ role: 'bot', text: data.text });
-
-                    updateChatUI();
-                    scrollToBottom(); // Scroll to bottom after the response is displayed
-                } else {
-                    console.error('Failed to send message to server');
-                    chatHistory.push({ role: 'bot', text: 'Yahh error, silahkan refresh atau coba beberapa saat lagi' });
-                    updateChatUI();
-                }
-            } catch (error) {
-                console.error('Error sending message:', error);
-                chatHistory.push({ role: 'bot', text: 'Yahh error, silahkan refresh atau coba beberapa saat lagi' });
-                updateChatUI();
-            }
-        }
-
-        textarea.value = '';
-        autoExpandTextarea();
-        autosize.update(textarea);
-    };
-
-    window.startNewChat = function () {
-        chatHistory.length = 0;
-        updateChatUI();
-        chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight;
-    };
-
-    function autoExpandTextarea() {
-        textarea.style.height = 'auto';
-        textarea.style.height = textarea.scrollHeight + 'px';
+      } catch (error) {
+        console.error('Error sending message:', error)
+        chatHistory.push({
+          role: 'bot',
+          text: 'Yahh error, silahkan refresh atau coba beberapa saat lagi',
+        })
+        updateChatUI()
+      }
     }
 
-    function insertNewLine() {
-        const cursorPosition = textarea.selectionStart;
-        const textBefore = textarea.value.substring(0, cursorPosition);
-        const textAfter = textarea.value.substring(cursorPosition);
+    textarea.value = ''
+    autoExpandTextarea()
+    autosize.update(textarea)
+  }
 
-        textarea.value = textBefore + '\n' + textAfter;
-        autosize.update(textarea);
+  window.startNewChat = function () {
+    chatHistory.length = 0
+    updateChatUI()
+    chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight
+  }
+
+  function autoExpandTextarea() {
+    textarea.style.height = 'auto'
+    textarea.style.height = textarea.scrollHeight + 'px'
+  }
+
+  function insertNewLine() {
+    const cursorPosition = textarea.selectionStart
+    const textBefore = textarea.value.substring(0, cursorPosition)
+    const textAfter = textarea.value.substring(cursorPosition)
+
+    textarea.value = textBefore + '\n' + textAfter
+    autosize.update(textarea)
+  }
+
+  function updateChatUI() {
+    chatMessagesContainer.innerHTML = ''
+
+    chatHistory.forEach((message) => {
+      displayMessage(message.role, message.text)
+    })
+
+    setTimeout(() => {
+      scrollToBottom()
+    }, 0)
+  }
+
+  function displayMessage(role, text) {
+    const messageDiv = document.createElement('div')
+    messageDiv.className = role === 'user' ? 'user-message' : 'bot-message'
+    messageDiv.innerText = text
+
+    const copyButton = document.createElement('button')
+    copyButton.className = 'copy-button'
+    copyButton.innerText = 'Copy'
+    copyButton.onclick = function () {
+      copyTextToClipboard(text)
     }
 
-    function updateChatUI() {
-        chatMessagesContainer.innerHTML = '';
+    messageDiv.appendChild(copyButton)
+    chatMessagesContainer.appendChild(messageDiv)
 
-        chatHistory.forEach((message) => {
-            displayMessage(message.role, message.text);
-        });
+    scrollToBottom()
+  }
 
-        setTimeout(() => {
-            scrollToBottom();
-        }, 0);
-    }
+  function copyTextToClipboard(text) {
+    const textarea = document.createElement('textarea')
+    textarea.value = text
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    document.body.removeChild(textarea)
+  }
 
-    function displayMessage(role, text) {
-        const messageDiv = document.createElement('div');
-        messageDiv.className = role === 'user' ? 'user-message' : 'bot-message';
-        messageDiv.innerText = text;
+  function scrollToTop() {
+    chatMessagesContainer.scrollTop = 0
+  }
 
-        const copyButton = document.createElement('button');
-        copyButton.className = 'copy-button';
-        copyButton.innerText = 'Copy';
-        copyButton.onclick = function () {
-            copyTextToClipboard(text);
-        };
+  function scrollToBottom() {
+    chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight
+  }
 
-        messageDiv.appendChild(copyButton);
-        chatMessagesContainer.appendChild(messageDiv);
+  function isUserLastSender() {
+    const lastMessage = chatHistory[chatHistory.length - 1]
+    return lastMessage && lastMessage.role === 'user'
+  }
 
-        scrollToBottom();
-    }
+  autosize(textarea)
 
-    function copyTextToClipboard(text) {
-        const textarea = document.createElement('textarea');
-        textarea.value = text;
-        document.body.appendChild(textarea);
-        textarea.select();
-        document.execCommand('copy');
-        document.body.removeChild(textarea);
-    }
-
-    function scrollToTop() {
-        chatMessagesContainer.scrollTop = 0;
-    }
-
-    function scrollToBottom() {
-        chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight;
-    }
-
-    function isUserLastSender() {
-        const lastMessage = chatHistory[chatHistory.length - 1];
-        return lastMessage && lastMessage.role === 'user';
-    }
-
-    autosize(textarea);
-
-    document.querySelector(".scroll-to-top").addEventListener("click", scrollToTop);
-});
+  document
+    .querySelector('.scroll-to-top')
+    .addEventListener('click', scrollToTop)
+})
--- server.js
+++ server.js
@@ -1,109 +1,126 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
 const {
   GoogleGenerativeAI,
   HarmCategory,
   HarmBlockThreshold,
   GoogleGenerativeAIResponseError,
-} = require('@google/generative-ai');
+} = require('@google/generative-ai')
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-const MODEL_NAME = 'gemini-pro';
-const API_KEY = 'AIzaSyDvB7PBnlSaeootaAoGwRBnwjuk5Ah80KY'; // Ganti dengan API Key Google Generative AI Anda
+const MODEL_NAME = 'gemini-pro'
+const API_KEY = 'AIzaSyDvB7PBnlSaeootaAoGwRBnwjuk5Ah80KY' // Ganti dengan API Key Google Generative AI Anda
 
-let server;
-let genAI;
-let model;
-let chat;
-const chatHistory = [];
+let server
+let genAI
+let model
+let chat
+const chatHistory = []
 
 const startServer = () => {
   server = app.listen(port, () => {
-    console.log(`Server berjalan di http://localhost:${port}`);
-  });
-};
+    console.log(`Server berjalan di http://localhost:${port}`)
+  })
+}
 
 const clearState = () => {
-  chatHistory.length = 0;
-  genAI = new GoogleGenerativeAI(API_KEY);
-  model = genAI.getGenerativeModel({ model: MODEL_NAME });
+  chatHistory.length = 0
+  genAI = new GoogleGenerativeAI(API_KEY)
+  model = genAI.getGenerativeModel({ model: MODEL_NAME })
   const generationConfig = {
     temperature: 0.5,
     topK: 20,
     topP: 1,
     maxOutputTokens: 3000,
-  };
+  }
 
   const safetySettings = [
-    { category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH },
-    { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH },
-    { category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH },
-    { category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH },
-  ];
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+    },
+  ]
 
   chat = model.startChat({
     generationConfig,
     safetySettings,
     history: chatHistory,
-  });
-};
+  })
+}
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.post('/sendMessage', async (req, res) => {
-  const userInput = req.body.userInput;
+  const userInput = req.body.userInput
 
   try {
-    const result = await chat.sendMessage(userInput);
-    const response = result.response;
+    const result = await chat.sendMessage(userInput)
+    const response = result.response
 
-    chatHistory.push({ role: 'user', parts: [{ text: userInput }] });
-    chatHistory.push({ role: 'model', parts: [{ text: response.text() }] });
+    chatHistory.push({ role: 'user', parts: [{ text: userInput }] })
+    chatHistory.push({ role: 'model', parts: [{ text: response.text() }] })
 
-    res.json({ text: response.text(), chatHistory });
+    res.json({ text: response.text(), chatHistory })
   } catch (error) {
     if (error instanceof GoogleGenerativeAIResponseError) {
-      console.error('Terjadi kesalahan pada GoogleGenerativeAI:', error.message);
-      console.log('Merestart server...');
+      console.error('Terjadi kesalahan pada GoogleGenerativeAI:', error.message)
+      console.log('Merestart server...')
       // Bersihkan state dan restart server
-      clearState();
+      clearState()
       server.close(() => {
-        startServer();
-      });
+        startServer()
+      })
     } else {
-      console.error('Terjadi kesalahan lain:', error.message);
-      res.status(500).json({ error: 'Terjadi kesalahan saat mengirim pesan.' });
+      console.error('Terjadi kesalahan lain:', error.message)
+      res.status(500).json({ error: 'Terjadi kesalahan saat mengirim pesan.' })
     }
   }
-});
+})
 
-startServer();
+startServer()
 
 process.on('uncaughtException', (err) => {
-  console.error('Terjadi pengecualian yang tidak tertangkap:', err);
-  console.log('Merestart server...');
+  console.error('Terjadi pengecualian yang tidak tertangkap:', err)
+  console.log('Merestart server...')
   // Bersihkan state dan restart server
-  clearState();
+  clearState()
   server.close(() => {
-    startServer();
-  });
-});
+    startServer()
+  })
+})
 
 process.on('unhandledRejection', (reason, promise) => {
-  console.error('Penolakan yang tidak tertangkap di:', promise, 'alasan:', reason);
-  console.log('Merestart server...');
+  console.error(
+    'Penolakan yang tidak tertangkap di:',
+    promise,
+    'alasan:',
+    reason
+  )
+  console.log('Merestart server...')
   // Bersihkan state dan restart server
-  clearState();
+  clearState()
   server.close(() => {
-    startServer();
-  });
-});
+    startServer()
+  })
+})
